52c3f131887277f26322679d53d5a457b9164a73
swamipeddineni <75420197+swamipeddineni@users.noreply.github.com>
2021-02-04 07:18:35 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://52.66.11.123:9000/',
+  serverUrl: 'http://13.233.3.29:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
